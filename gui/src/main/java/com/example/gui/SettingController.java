bdb10fd7debf9484c47da45134c02a71b03c6256
Negar Kakaei <92810954+negarkakaei@users.noreply.github.com>
2022-07-11 02:20:22 +0430

    after merge

--- gui/src/main/java/com/example/gui/SettingController.java
+++ gui/src/main/java/com/example/gui/SettingController.java
@@ -8,23 +8,17 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-
 import java.awt.*;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.URL;
 import java.nio.file.Paths;
-import java.util.ResourceBundle;
 import static java.nio.file.Files.readAllBytes;
 
 public class SettingController {
