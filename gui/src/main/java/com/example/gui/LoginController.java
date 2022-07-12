3941d742fb87a9ff9d2f86d183ad42236e8f4ff1
Negar Kakaei <92810954+negarkakaei@users.noreply.github.com>
2022-07-12 23:25:35 +0430

    javadoc login

--- gui/src/main/java/com/example/gui/LoginController.java
+++ gui/src/main/java/com/example/gui/LoginController.java
@@ -20,61 +20,62 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-
+/**
+ * this class is responsible for handling events in login-view.fxml file such as pressing buttons etc.
+ */
 public class LoginController {
 
-    private  ObjectOutputStream out;
-    private  ObjectInputStream in;
-    private  ObjectOutputStream fout;
-    private  ObjectInputStream fin;
+    private ObjectOutputStream out;
+    private ObjectInputStream in;
+    private ObjectOutputStream fout;
+    private ObjectInputStream fin;
 
     private Stage stage;
     private Scene scene;
 
     @FXML
     private TextField username;
-
     @FXML
     private PasswordField password;
-
     @FXML
     private Button login_button;
-
     @FXML
     private Text login_signup;
-
     @FXML
     private Text login_username_description;
 
-    public LoginController(){
-        ;
-    }
+    public LoginController() {}
 
 
-    public LoginController(ObjectInputStream in, ObjectOutputStream out,ObjectInputStream fin, ObjectOutputStream fout){
+    public LoginController(ObjectInputStream in, ObjectOutputStream out, ObjectInputStream fin, ObjectOutputStream fout) {
         this.in = in;
         this.out = out;
         this.fin = fin;
         this.fout = fout;
     }
 
+    /**
+     * handles the event when the login button is pressed
+     * get the information from text fields, send them to server, checks if the user with given information exists
+     * changes the scene to friends view if the information is correct and shows an error otherwise
+     * @param event
+     */
     @FXML
     public void loginOnButton(Event event) {
         try {
-            out.writeObject(Command.login(username.getText(),password.getText()));
+            out.writeObject(Command.login(username.getText(), password.getText()));
             Data data = (Data) in.readObject();
-            if(!((boolean) data.getPrimary())){
+            if (!((boolean) data.getPrimary())) {
                 System.out.println("not correct");
                 login_username_description.setText("wrong username or password");
                 login_username_description.setFill(Color.RED);
-            }
-            else {
+            } else {
                 System.out.println("correct");
                 //and other actions
                 FXMLLoader fxmlLoader = new FXMLLoader(LoginController.class.getResource("friends-view.fxml"));
-                FriendsController friendsController = new FriendsController(in,out,fin,fout,username.getText());
+                FriendsController friendsController = new FriendsController(in, out, fin, fout, username.getText());
                 fxmlLoader.setController(friendsController);
-                stage = (Stage)(((Node) event.getSource()).getScene().getWindow());
+                stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
                 try {
                     scene = new Scene(fxmlLoader.load(), 1000, 600);
                 } catch (IOException e) {
@@ -88,11 +89,16 @@ public class LoginController {
         }
     }
 
+    /**
+     * handles the event when the blue signup button is pressed
+     * changes the scene to signup view in the same stage
+     * @param event
+     */
     @FXML
-    public void changeToSignUp(Event event){
+    public void changeToSignUp(Event event) {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("signup-view.fxml"));
-        fxmlLoader.setController(new SignupController(in,out,fin,fout));
-        stage = (Stage)(((Node) event.getSource()).getScene().getWindow());
+        fxmlLoader.setController(new SignupController(in, out, fin, fout));
+        stage = (Stage) (((Node) event.getSource()).getScene().getWindow());
         try {
             scene = new Scene(fxmlLoader.load(), 1000, 600);
         } catch (IOException e) {
